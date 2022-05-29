73f3a4b937f5b3b3a606a950c7f5cfdff961c92f
TejasJogi <tejasjogi1995@gmail.com>
2022-05-29 15:27:33 +0530

    UserLogin view added in views.py

--- account/views.py
+++ account/views.py
@@ -1,17 +1,41 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from account.serializers import UserRegisterSerializer
+from account.serializers import UserRegisterSerializer, UserLoginSerializer
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 class UserResgister(APIView):
-    def post(self, request, format=None):
+    def post(self, request, format='JSON'):
         serializer = UserRegisterSerializer(data=request.data)
 
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
             return Response({
-                "message": "You are successfully registered."
-            }, status=status.HTTP_201_CREATED)
+                "message": "You are successfully registered.",
+                "status": status.HTTP_201_CREATED,
+                "user": {
+                    "username": request.data['username'],
+                    "email": request.data['email'],
+                    "phone": request.data['phone'],
+                }
+            },status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLogin(APIView):
+    def post(self, request, format='JSON'):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                return Response({
+                    "message": "Login Successfully."
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    "errors": {'non_field_errors': ['Email or Password is not Valid']}
+                }, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
