101902907adbae943fa61d8cee26d696c5665e06
TejasJogi <tejasjogi1995@gmail.com>
2022-05-29 16:38:21 +0530

    renderer class add in views.py

--- account/views.py
+++ account/views.py
@@ -3,10 +3,13 @@ from rest_framework import status
 from rest_framework.views import APIView
 from account.serializers import UserRegisterSerializer, UserLoginSerializer
 from django.contrib.auth import authenticate
+from account.renderers import Renderer
 
 # Create your views here.
 
+
 class UserResgister(APIView):
+    renderer_classes = [Renderer]
     def post(self, request, format='JSON'):
         serializer = UserRegisterSerializer(data=request.data)
 
@@ -20,10 +23,12 @@ class UserResgister(APIView):
                     "email": request.data['email'],
                     "phone": request.data['phone'],
                 }
-            },status=status.HTTP_201_CREATED)
+            }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class UserLogin(APIView):
+    renderer_classes = [Renderer]
     def post(self, request, format='JSON'):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -36,6 +41,5 @@ class UserLogin(APIView):
                 }, status=status.HTTP_200_OK)
             else:
                 return Response({
-                    "errors": {'non_field_errors': ['Email or Password is not Valid']}
-                }, status=status.HTTP_404_NOT_FOUND)
+                    'non_field_errors': 'Email or Password is not Valid'}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
