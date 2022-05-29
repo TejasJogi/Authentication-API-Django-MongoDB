c498291418e9740cb01a85364b86b9f224b4d708
TejasJogi <tejasjogi1995@gmail.com>
2022-05-29 20:45:50 +0530

    database chenged to MongoDB

--- djangoauthapi/settings.py
+++ djangoauthapi/settings.py
@@ -83,10 +83,23 @@ WSGI_APPLICATION = 'djangoauthapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'invoice-tejas',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb+srv://admin:admin@cluster0.cnochr3.mongodb.net/?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE',
+            'username': 'admin',
+            'password': 'admin',
+        }
     }
 }
 
