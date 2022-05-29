5d8429bd1bdefa1edb99a6bff9e5f13e1035abbb
TejasJogi <tejasjogi1995@gmail.com>
2022-05-29 18:18:49 +0530

    Implement JWT Token manually in views.py

--- account/views.py
+++ account/views.py
@@ -4,17 +4,28 @@ from rest_framework.views import APIView
 from account.serializers import UserRegisterSerializer, UserLoginSerializer
 from django.contrib.auth import authenticate
 from account.renderers import Renderer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
+
+# Generate token manually
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 
 class UserResgister(APIView):
     renderer_classes = [Renderer]
-    def post(self, request, format='JSON'):
+    def post(self, request, format=None):
         serializer = UserRegisterSerializer(data=request.data)
 
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
+            token = get_tokens_for_user(user)
             return Response({
                 "message": "You are successfully registered.",
                 "status": status.HTTP_201_CREATED,
@@ -22,22 +33,25 @@ class UserResgister(APIView):
                     "username": request.data['username'],
                     "email": request.data['email'],
                     "phone": request.data['phone'],
-                }
+                },
+                "token": token
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserLogin(APIView):
     renderer_classes = [Renderer]
-    def post(self, request, format='JSON'):
+    def post(self, request, format=None):
         serializer = UserLoginSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             email = serializer.data.get('email')
             password = serializer.data.get('password')
             user = authenticate(email=email, password=password)
             if user is not None:
+                token = get_tokens_for_user(user)
                 return Response({
-                    "message": "Login Successfully."
+                    "message": "Login Successfully.",
+                    "token": token
                 }, status=status.HTTP_200_OK)
             else:
                 return Response({
