da9d95318a846e75d9a699ebf9491e698c1a3409
TejasJogi <tejasjogi1995@gmail.com>
2022-05-29 15:29:07 +0530

    url for login added in app urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from account.views import UserResgister
+from account.views import UserResgister, UserLogin
 
 
 urlpatterns = [
-    path('register', UserResgister.as_view(), name='register')
+    path('register', UserResgister.as_view(), name='register'),
+    path('login', UserLogin.as_view(), name='login'),
 ]
